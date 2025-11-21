ff292735fbf4f483770d191c9b4671a8ec118e73
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-21 08:57:00 -0600

    Added Mode Login And SingUp

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import AuthForm from '../components/auth-form';
 
-export default function Home() {
-  return <AuthForm />;
+export default function Home({ searchParams }: { searchParams: { mode: string } }) {
+  const formMode = searchParams.mode || 'login';
+  
+  return <AuthForm mode={formMode} />;
 }
